c7a5d4340a706ff1fd51275da7930922d47e46b7
eric-yijuin-lin <63579803+eric-yijuin-lin@users.noreply.github.com>
2024-03-13 21:24:15 +0800

    Create game_assets.py

---
+++ game_assets.py
@@ -0,0 +1,24 @@
+from pygame.image import load
+
+card_images = [
+    load("img/stone_card_1.png"),
+    load("img/water_card_1.png"),
+    load("img/wood_card_1.png"),
+    load("img/food_card_1.png"),
+    load("img/metal_card_1.png"),
+    load("img/jewel_card_1.png"),
+]
+
+icon_images = [
+    load("img/stone_icon.png"),
+    load("img/water_icon.png"),
+    load("img/wood_icon.png"),
+    load("img/food_icon.png"),
+    load("img/metal_icon.png"),
+    load("img/jewel_icon.png"),
+]
+
+tile_images = [
+  load("img/mud_tile_revealed.png"),
+  load("img/mud_tile.png"),
+]
