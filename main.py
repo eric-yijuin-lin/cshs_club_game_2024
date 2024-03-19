52ef6cdd0f89aa08021ecd18a7d83c05be1a07a2
Eric Lin <eric.yijuin.lin@gmail.com>
2024-03-19 15:39:44 +0800

    refactor mine game functions into mine game manager

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from game_assets import *
 from game_data import UserResource
 from sprite import GameSprite
 
-
+CHILD_SCENE_RECT = (0, 70, 420, 370)
 
 pygame.init()
 main_scene = pygame.display.set_mode((420, 500))
@@ -15,19 +15,19 @@ font = pygame.font.SysFont('arial', 16)
 clock = pygame.time.Clock()
 
 user_resource = UserResource()
-mine_game_manager = MineGameManager()
+mine_game_manager = MineGameManager(CHILD_SCENE_RECT)
 synth_manager = SynthesizeManager()
 
-mine_button = GameSprite(button_images[0], Rect(70, 450, 79, 35))
-synth_button = GameSprite(button_images[1], Rect(160, 450, 79, 35))
-rank_button = GameSprite(button_images[2], Rect(250, 450, 79, 35))
+mine_button = GameSprite(button_images[0], Rect(70, 455, 79, 35))
+synth_button = GameSprite(button_images[1], Rect(160, 455, 79, 35))
+rank_button = GameSprite(button_images[2], Rect(250, 455, 79, 35))
 
 fps = 40
 
 def draw_recource_icons():
     for i in range(len(user_resource.recoures)):
         x = 80 + (i % 3) * 100
-        y = 10 + (i // 3) * 40
+        y = 5 + (i // 3) * 30
         main_scene.blit(icon_images[i], (x, y))
     draw_resource_amount()
 
@@ -36,44 +36,13 @@ def draw_resource_amount() -> None:
         resource = user_resource.recoures[i]
         text = font.render(str(resource), True, (0, 0, 0))
         x = 120 + (i % 3) * 100
-        y = 20 + (i // 3) * 40
+        y = 5 + (i // 3) * 35
         main_scene.blit(text, (x, y))
-
-def draw_mine_map(mine_map: list) -> None:
-    for tile in mine_map:
-        img = tile_images[0] if tile.is_revealed else tile_images[1]
-        main_scene.blit(img, tile.rect)
-
-def reveal_mine_cell(position: tuple) -> MineMapCell:
-    for cell in mine_game_manager.mine_map:
-        if cell.rect.collidepoint(position):
-            cell.is_revealed = True
-            mine_game_manager.pickaxe_count -= 1
-            # print(cell.rect)
-            # print(cell.resource_type)
-            # print(cell.recource_level)
-            return cell
-    return None
-
-def draw_pickaxe(count: int) -> None:
-    text = font.render(str(count), True, (0, 0, 0))
-    main_scene.blit(pickaxe_image, (180, 405))
-    main_scene.blit(text, (220, 410))
-
 
 def draw_buttons() -> None:
     main_scene.blit(mine_button.image, mine_button.rect)
     main_scene.blit(synth_button.image, synth_button.rect)
     main_scene.blit(rank_button.image, rank_button.rect)
-
-def draw_card(mine_cell: MineMapCell):
-    if mine_cell is None or mine_cell.resource_type == ResourceType.Nothing:
-        return
-    idx = mine_cell.resource_type.value - 1
-    img = card_images[idx]
-    main_scene.blit(img, (80, 80))
-    pygame.display.update()
-    time.sleep(0.5)
 
 def process_button_click(posision: tuple) -> None:
     if synth_button.is_clicked(posision):
@@ -90,19 +59,13 @@ running = True
 while running:
     main_scene.fill((200, 200, 200))
     draw_recource_icons()
-    draw_mine_map(mine_game_manager.mine_map)
-    draw_pickaxe(mine_game_manager.pickaxe_count)
     draw_buttons()
 
-    ev = pygame.event.get()
-    for e in ev:
-        if e.type == pygame.MOUSEBUTTONUP:
-            pos = pygame.mouse.get_pos()
-            revealed_cell = reveal_mine_cell(pos)
-            process_button_click(pos)
-            draw_card(revealed_cell)
-      
-    for event in pygame.event.get():
+    events = pygame.event.get()
+    frame = mine_game_manager.process_frame(events)
+    main_scene.blit(frame, (CHILD_SCENE_RECT[0], CHILD_SCENE_RECT[1]))
+
+    for event in events:
         if event.type == pygame.QUIT:
             running = False
 
--- mine_game.py
+++ mine_game.py
@@ -1,6 +1,7 @@
+import pygame
 from enum import Enum
 from random import randint, uniform
-from pygame import Rect
+from game_assets import tile_images, pickaxe_image, card_images
 
 class ResourceType(Enum):
     Nothing = 0
@@ -11,6 +12,11 @@ class ResourceType(Enum):
     Metal = 5
     Jewel = 6
 
+class GameStatus(Enum):
+    Hiden = 0
+    Running = 1
+    Pause = 2
+
 MINE_MAP_ROW_SIZE = 5
 MINE_MAP_COLUMN_SIZE = 6
 MINE_TILE_WIDTH = 50
@@ -20,6 +26,7 @@ MAX_PICKAXE_COUNT = MINE_MAP_ROW_SIZE * MINE_MAP_COLUMN_SIZE // 2
 RESOURCE_CELL_RATE = 0.6
 RESOURCE_LEVEL_RANGE = (0.9, 0.7, 0)
 
+
 class MineMapCell:
     def __init__(self, cell_index: int) -> None:
         self.recource_level = 0
@@ -27,9 +34,9 @@ class MineMapCell:
         self.resource_type = ResourceType.Nothing
         index_x = cell_index % MINE_MAP_ROW_SIZE
         index_y = cell_index // MINE_MAP_ROW_SIZE
-        self.rect = Rect(
-            75 + index_x * MINE_TILE_WIDTH, 
-            100 + index_y * MINE_TILE_HEIGHT,
+        self.rect = pygame.Rect(
+            80 + index_x * MINE_TILE_WIDTH, 
+            10 + index_y * MINE_TILE_HEIGHT,
             MINE_TILE_WIDTH,
             MINE_TILE_HEIGHT
         )
@@ -56,14 +63,78 @@ class MineMapCell:
                 break
 
 class MineGameManager:
-    def __init__(self) -> None:
+    def __init__(self, rect: tuple) -> None:
+        self.canvas_rect = rect
+        self.game_status = GameStatus.Running
+        self.canvas = pygame.Surface(
+            (self.canvas_rect[2],
+             self.canvas_rect[3])
+        )
         self.pickaxe_count = MAX_PICKAXE_COUNT
         self.mine_map = self.new_mine_map()
+        self.font = pygame.font.SysFont('arial', 16)
+        self.card_frame_delay = 60 # with fps=40, means pause 1.5 seconds
 
-    def new_mine_map(self) -> list:
+    def new_mine_map(self) -> list[MineMapCell]:
         mine_map = []
         for i in range(MINE_MAP_ROW_SIZE * MINE_MAP_COLUMN_SIZE):
             mine_map_cell = MineMapCell(i)
             mine_map_cell.roll_cell()
             mine_map.append(mine_map_cell)
         return mine_map
+
+    def blit_mine_map(self) -> None:
+        for tile in self.mine_map:
+            img = tile_images[0] if tile.is_revealed else tile_images[1]
+            self.canvas.blit(img, tile.rect)
+    
+    def reveal_mine_cell(self, position: tuple) -> MineMapCell:
+        for cell in self.mine_map:
+            if cell.rect.collidepoint(position):
+                cell.is_revealed = True
+                self.pickaxe_count -= 1
+                # print(cell.rect)
+                # print(cell.resource_type)
+                # print(cell.recource_level)
+                return cell
+        return None
+
+    def blit_pickaxe(self) -> None:
+        text = self.font.render(str(self.pickaxe_count), True, (0, 0, 0))
+        self.canvas.blit(pickaxe_image, (180, 315))
+        self.canvas.blit(text, (220, 320))
+
+    def blit_card(self, mine_cell: MineMapCell):
+        if mine_cell is None or mine_cell.resource_type == ResourceType.Nothing:
+            return
+        idx = mine_cell.resource_type.value - 1
+        img = card_images[idx]
+        self.canvas.blit(img, (80, 0))
+        self.card_frame_delay = 60
+        self.game_status = GameStatus.Pause
+
+    def process_frame(self, events: list) -> pygame.Surface:
+        if self.game_status == GameStatus.Hiden:
+            return None
+        if self.game_status == GameStatus.Pause:
+            self.card_frame_delay -= 1
+            if self.card_frame_delay <= 0:
+                self.game_status = GameStatus.Running
+            return self.canvas
+        if self.game_status == GameStatus.Running:
+            self.canvas.fill((255, 255, 255))
+            self.blit_mine_map()
+            self.blit_pickaxe()
+            for e in events:
+                if e.type == pygame.MOUSEBUTTONUP:
+                    main_screen_position = pygame.mouse.get_pos()
+                    child_scene_position = self.get_child_scene_position(main_screen_position)
+                    revealed_cell = self.reveal_mine_cell(child_scene_position)
+                    self.blit_card(revealed_cell)
+            return self.canvas
+        
+    def get_child_scene_position(self, main_screen_position: tuple) -> tuple:
+        return (
+            main_screen_position[0] - self.canvas_rect[0],
+            main_screen_position[1] - self.canvas_rect[1] 
+        )
