35a5d47bd55617fd87d19158f3adbcc2e3ec9f5a
Eric Lin <eric.yijuin.lin@gmail.com>
2024-05-03 08:44:00 +0800

    add mysql db

--- database/manager.py
+++ database/manager.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+import json
 import gspread
-
+import pymysql
 from common.game_data import Card
 
 class IDatabaseManager:
@@ -19,7 +20,7 @@ class IDatabaseManager:
 class GoogleSheetDatabase(IDatabaseManager):
     def __init__(self, credential: str, user_id: str) -> None:
         super().__init__()
-        self.user: dict = {}
+        self.user_info: dict = {}
         self.__init_work_sheets(credential)
         self.__init_user(user_id)
     def __init_work_sheets(self, credential_path: str) -> None:
@@ -30,7 +31,7 @@ class GoogleSheetDatabase(IDatabaseManager):
     def __init_user(self, user_id: str) -> None:
         for user in self.user_sheet.get_all_records():
             if user_id == user["USER_ID"]:
-                self.user = user
+                self.user_info = user
                 return
         raise ValueError("user_id 不存在, 請檢查是否有用 MD5 加密學號，然後取代掉 user_id 變數的值")
     def insert_craft_record(self, card: Card) -> None:
@@ -39,8 +40,8 @@ class GoogleSheetDatabase(IDatabaseManager):
         # 合成物品	組別	姓名	日期	時間
         row = [
             card.name,
-            self.user["組別"],
-            self.user["姓名"],
+            self.user_info["組別"],
+            self.user_info["姓名"],
             date_str,
             time_str
         ]
@@ -48,3 +49,68 @@ class GoogleSheetDatabase(IDatabaseManager):
             self.record_sheet.insert_row(row, 2)
         except Exception as ex:
             print("無法新增合成紀錄，錯誤訊息：", str(ex))
+
+class MySQLDatabase(IDatabaseManager):
+    def __init__(self, credential_path: str, user_info: dict) -> None:
+        super().__init__()
+        self.__user_info = user_info
+        self.__credential = {}
+        with open(credential_path) as f:
+            self.__credential = json.load(f)
+        self.__connection = self.__new_connection()
+    
+    def __new_connection(self) -> pymysql.Connection:
+        return pymysql.connect(
+            host = self.__credential["host"],
+            port = self.__credential["port"],
+            user = self.__credential["user"],
+            password = self.__credential["password"],
+            database = self.__credential["database"],
+        )
+    
+    def __check_connection(self) -> None:
+        if not self.__connection or not self.__connection.open:
+            self.__connection = self.__new_connection()
+
+    def insert_user_item(self, item_id: str, item_name: str) -> None:
+        sql = """
+            INSERT INTO `cshs_db`.`user_items`
+                (`user_id`,
+                `user_name`,
+                `item_id`,
+                `item_name`,
+                `group_name`)
+            VALUES
+                (%s,%s,%s,%s,%s);
+        """
+        try:
+            self.__check_connection()
+            cursor = self.__connection.cursor()
+            with cursor:
+                params = (
+                    self.__user_info["USER_ID"],
+                    self.__user_info["姓名"],
+                    item_id,
+                    item_name,
+                    self.__user_info["組別"],
+                )
+                cursor.execute(sql, params)
+            self.__connection.commit()
+        except Exception as ex:
+            print("Failed to insert item: ", ex)
+            if self.__connection:
+                self.__connection.close()
+
+    def test(self) -> None:
+        sql = """
+            SELECT * FROM `cshs_db`.`user_items`LIMIT 100;
+        """
+        self.__check_connection()
+        cursor = self.__connection.cursor()
+        with cursor:
+            params = (
+            )
+            cursor.execute(sql, params)
+            data = cursor.fetchall()
+            for d in data:
+                print(d)
